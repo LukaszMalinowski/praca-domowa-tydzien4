48e09f15b423be55a07ec04f1aa60d2325de8d7d
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-05-03 19:12:13 +0200

    Fixed formatting

--- src/main/java/pl/lukaszmalina/tydzien4/gui/CarGui.java
+++ src/main/java/pl/lukaszmalina/tydzien4/gui/CarGui.java
@@ -1,27 +1,19 @@
 package pl.lukaszmalina.tydzien4.gui;
 
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-import pl.lukaszmalina.tydzien4.entity.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.lukaszmalina.tydzien4.service.CarService;
 
-import java.util.LinkedHashMap;
-
-@Route("cars")
+@Route ("cars")
 public class CarGui extends VerticalLayout {
 
-    public CarGui() {
-        Button button = new Button("Fetch");
-        button.addClickListener(event -> {
-            RestTemplate restTemplate = new RestTemplate();
-            LinkedHashMap entity = restTemplate.getForObject("http://localhost:8080/api/cars", LinkedHashMap.class);
-            LinkedHashMap content = (LinkedHashMap) entity.get("content");
-            System.out.println(content);
-        });
-        add(button);
+    CarService service;
+
+    @Autowired
+    public CarGui(CarService service) {
+        this.service = service;
+
+        System.out.println(service.getCars());
     }
 }
