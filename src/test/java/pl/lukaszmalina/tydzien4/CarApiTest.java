6ce3fff62df10047a66a63379805576ec3d23648
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-05-05 22:54:35 +0200

    Added test case

--- src/test/java/pl/lukaszmalina/tydzien4/CarApiTest.java
+++ src/test/java/pl/lukaszmalina/tydzien4/CarApiTest.java
@@ -1,13 +1,19 @@
 package pl.lukaszmalina.tydzien4;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import pl.lukaszmalina.tydzien4.service.CarService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CarApiTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturn200WhenGettingAllCars() {
+    }
 }
